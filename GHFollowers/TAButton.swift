06dd37fe71db73974feece63dea9a31f980315d0
Christopher Aronson <christopheraronson@icloud.com>
2020-01-13 18:37:57 -0800

    Created Custom Button Class

---
+++ GHFollowers/TAButton.swift
@@ -0,0 +1,40 @@
+//
+//  TAButton.swift
+//  GHFollowers
+//
+//  Created by Christopher Aronson on 1/13/20.
+//  Copyright © 2020 Christopher Aronson. All rights reserved.
+//
+
+import UIKit
+
+class TAButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) was not implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        
+        configure()
+    }
+    
+    private func configure() {
+        
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
