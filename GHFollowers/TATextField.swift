afef8797e4e160ba0f6d93877b3753bfd98f4479
Christopher Aronson <christopheraronson@icloud.com>
2020-01-13 18:47:17 -0800

    Created Custom TextField Class

---
+++ GHFollowers/TATextField.swift
@@ -0,0 +1,44 @@
+//
+//  TATextField.swift
+//  GHFollowers
+//
+//  Created by Christopher Aronson on 1/13/20.
+//  Copyright © 2020 Christopher Aronson. All rights reserved.
+//
+
+import UIKit
+
+class TATextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Enter a Username"
+    }
+    
+}
