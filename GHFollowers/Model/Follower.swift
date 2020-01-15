32381ab56d1f48294944f52efd3a5b221dd847cb
Christopher Aronson <christopheraronson@icloud.com>
2020-01-14 18:50:48 -0800

    Created Follower Model

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Christopher Aronson on 1/14/20.
+//  Copyright © 2020 Christopher Aronson. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
